cb6fa598d93005cbc685a832bb904919f2823e3f
ChrisCordoba777 <143844083+ChrisCordoba777@users.noreply.github.com>
2024-08-30 02:29:27 -0400

    adding starting project

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Chris Cordoba",
+    username: "@IAmChrisCordoba",
+    email: "iamchriscordoba@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Perla Hernandez",
+    username: "@P.erlaah",
+    email: "perlita@gmail.com",
+  },
+  {
+    id: 3,
+    name: "Ixchel Crowe",
+    username: "@Ixchy",
+    email: "ixchel896@gmail.com",
+  },
+  {
+    id: 4,
+    name: "Izzy Crowe",
+    username: "@IzzyPizzy",
+    email: "isabellacrowe@gmail.com",
+  },
+  {
+    id: 5,
+    name: "Chris Lopez",
+    username: "@Feniko",
+    email: "fenikobeatz@gmail.com",
+  },
+  {
+    id: 6,
+    name: "Jose Duran",
+    username: "@Voyso",
+    email: "ruso_maricon@gmail.com",
+  },
+  {
+    id: 7,
+    name: "Brenda Blanco",
+    username: "@Bblanco",
+    email: "brendita1@gmail.com",
+  },
+  {
+    id: 8,
+    name: "Alma Rosa",
+    username: "@Almita",
+    email: "rossy_cordoba@gmail.com",
+  },
+  {
+    id: 9,
+    name: "Joey Cifuentes",
+    username: "@JoeyBCR",
+    email: "homo-thug@gmail.com",
+  },
+  {
+    id: 10,
+    name: "Isabella Khaled",
+    username: "@iisabellakhaled",
+    email: "habibi-911@gmail.com",
+  },
+];
